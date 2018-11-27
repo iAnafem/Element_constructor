c796c95dc765e720d1420478e701fa507d51b380
Dmitriy <45180222+iAnafem@users.noreply.github.com>
2018-11-27 16:32:08 +0300

    front and top view are accomplished

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ import openpyxl
 
 
 acad = Autocad(create_if_not_exists=True)
-acad.Application.Documents.Open("D:\\IT\\Bridge_truss\\pattern.dwg")
+# acad.Application.Documents.Open("D:\\IT\\Bridge_truss\\pattern.dwg")
 acad.prompt("Hello, Autocad from Python\n")
 print(acad.doc.Name)
 
@@ -37,11 +37,10 @@ def dim_aligned(point1, point2, scale, indent_x=0, indent_y=0, start_point_x=0, 
 def holes(diameter: str, start_point_x, start_point_y, holes_grid, move_x=0, move_y=0):
 
     for x in range(0, len(holes_grid), 2):
-        if holes_grid[x] != 0:
-            block = acad.model.InsertBlock(APoint(start_point_x + holes_grid[x],
+        block = acad.model.InsertBlock(APoint(start_point_x + holes_grid[x],
                                                   start_point_y + holes_grid[x+1]),
-                                           "hole_" + diameter, 1, 1, 1, 0)
-            block.move(APoint(0, 0), APoint(move_x, move_y))
+                                           "hole_" + str(diameter), 1, 1, 1, 0)
+        block.move(APoint(0, 0), APoint(move_x, move_y))
 
 
 def i_beam(l_i, b1_i, tw1_i, b2_i, tw2_i, h_i, t_i, sheet):
@@ -83,21 +82,31 @@ def i_beam(l_i, b1_i, tw1_i, b2_i, tw2_i, h_i, t_i, sheet):
 
     p9 = array.array('d', [0, b2_i, 0])
     p10 = array.array('d', [l_i, b2_i, 0])
-    p11 = array.array('d', [-100, b2_i / 2, 0])
-    p12 = array.array('d', [l_i + 100, b2_i / 2, 0])
-
+    p11 = array.array('d', [-1*scale_top, b2_i / 2, 0])
+    p12 = array.array('d', [l_i + 1*scale_top, b2_i / 2, 0])
+    p13 = array.array('d', [0, b2_i / 2 - t_i/2, 0])
+    p14 = array.array('d', [l_i, b2_i / 2 - t_i/2, 0])
+    p15 = array.array('d', [0, b2_i / 2 + t_i / 2, 0])
+    p16 = array.array('d', [l_i, b2_i / 2 + t_i / 2, 0])
     poly_t_1 = acad.model.AddPolyline(p1 + p2 + p10 + p9 + p1)
     poly_t_2 = acad.model.AddPolyline(p11 + p12)
-
+    poly_t_3 = acad.model.AddPolyline(p13 + p14)
+    poly_t_4 = acad.model.AddPolyline(p15 + p16)
     poly_t_1.layer = "Основная 0.25"
     poly_t_2.layer = "Ось"
-
+    poly_t_3.layer = "Пунктир"
+    poly_t_4.layer = "Пунктир"
     poly_t_1.move(APoint(0, 0), APoint(0, offset_view_y))
     poly_t_2.move(APoint(0, 0), APoint(0, offset_view_y))
+    poly_t_3.move(APoint(0, 0), APoint(0, offset_view_y))
+    poly_t_4.move(APoint(0, 0), APoint(0, offset_view_y))
 
     print("Creating of bolt holes grid ...")
 
     # Making holes grid for chords:
+
+    # Bolt holes diameter
+    diameter = sheet["K2"].value
 
     # Left grid
 
@@ -114,6 +123,15 @@ def i_beam(l_i, b1_i, tw1_i, b2_i, tw2_i, h_i, t_i, sheet):
         holes_right[i] = -holes_left[i]
     for i in range(1, len(holes_left), 2):
         holes_right[i] = holes_left[i]
+
+    # Middle grid
+
+    holes_mid = []
+    for i in range(39, 45):
+        for j in range(2, 16):
+            if sheet.cell(row=i, column=j).value is not None:
+                holes_mid.append(sheet.cell(row=i, column=j).value)
+    start_point_x = sheet["B37"].value
 
     # Making holes grid for wall
 
@@ -133,11 +151,44 @@ def i_beam(l_i, b1_i, tw1_i, b2_i, tw2_i, h_i, t_i, sheet):
     for i in range(1, len(holes_wall_left), 2):
         holes_wall_right[i] = holes_wall_left[i]
 
-    holes("25", 0, 0, holes_left, move_y=offset_view_y)
-    holes("25", l_i, 0, holes_right, move_y=offset_view_y)
-    holes("25", 0, tw2_i, holes_wall_left)
-    holes("25", l_i, tw2_i, holes_wall_right)
+    # Creating of bolt holes grid
+    holes(diameter, 0, 0, holes_left, move_y=offset_view_y)
+    holes(diameter, l_i, 0, holes_right, move_y=offset_view_y)
+    holes(diameter, 0, tw2_i, holes_wall_left)
+    holes(diameter, l_i, tw2_i, holes_wall_right)
+    holes(diameter, start_point_x, 0, holes_mid, move_y=offset_view_y)
     # holes("25", 0, 0, holes_grid3, move_y=offset_view_y)
     # holes("25", 0, 0, holes_grid4, move_y=offset_view_y)
     dim_aligned(p1, p2, scale_top, indent_y=-10, move_y=offset_view_y)
+
+    # Bolt holes side view
+
+    h_s_x = []
+    # holes_side front view
+    for i in range(68, 88):
+        if sheet.cell(row=i, column=2).value is not None:
+            h_s_x.append(sheet.cell(row=i, column=2).value)
+    for i in range(len(h_s_x)-1):
+        p_x1 = array.array('d', [h_s_x[i], 0, 0])
+        if h_s_x[i] > 0:
+            hole_center1 = acad.model.AddLine(APoint(h_s_x[i], -1 * scale_front),
+                                              APoint(h_s_x[i], tw2_i + 1 * scale_front))
+            hole_line1 = acad.model.AddLine(APoint(float(h_s_x[i]-diameter/2), 0),
+                                            APoint(float(h_s_x[i]-diameter/2), tw2_i))
+            hole_line2 = acad.model.AddLine(APoint(float(h_s_x[i]+diameter/2), 0),
+                                            APoint(float(h_s_x[i]+diameter/2), tw2_i))
+            hole_center1.layer = "Ось"
+            hole_line1.layer = "Пунктир"
+            hole_line2.layer = "Пунктир"
+            hole_center1.copy().move(APoint(0, 0), APoint(0, h_i - tw1_i))
+            hole_line1.copy().move(APoint(0, 0), APoint(0, h_i - tw1_i))
+            hole_line2.copy().move(APoint(0, 0), APoint(0, h_i - tw1_i))
+        p_x2 = array.array('d', [h_s_x[i+1], 0, 0])
+        dim_aligned(p_x1, p_x2, scale_front, indent_y=-10, move_y=offset_view_y)
+
+    h_s_s = []
+    # holes_side side view
+
+    for i in range(18, 22, 2):
+        h_s_s = [].append(sheet.cell(row=i, column=2).value)
 # acad.ActiveDocument.SaveAs('D:\\IT\\Bridge_truss\\1.dwg')
